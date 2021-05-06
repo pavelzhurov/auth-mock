daef5984114a1626fd0866a2e68eb041f7a1b144
pavelzhurov <jur96@mail.ru>
2021-05-06 22:17:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-cors = CORS(app, resources={r"/api/*": {"origins": "app.example.com"}})
+cors = CORS(app, resources={r"/api/*": {"origins": "https://app.example.com"}})
 
 
 @app.route('/')
